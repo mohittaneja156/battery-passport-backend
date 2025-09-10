41a57bc2d81b91ee78543ca1e609446ae4bcc0d5
Mohit Taneja <mohittaneja156@gmail.com>
2025-09-11 03:46:32 +0530

    fix cicd

--- services/auth-service/jest.config.js
+++ services/auth-service/jest.config.js
@@ -1,5 +1,13 @@
 export default {
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.js'],
-  transform: {}
+  transform: {},
+  lint: {
+    '*': [
+      'cd services/auth-service && npm run lint',
+      'cd ../passport-service && npm run lint',
+      'cd ../document-service && npm run lint',
+      'cd ../notification-service && npm run lint'
+    ]
+  }
 }
